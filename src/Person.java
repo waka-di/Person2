20a7c7d3ff1b82f039f27d1fe4dc776342a1a370
waka-di <miho.1225.miho@icloud.com>
2025-08-10 15:45:19 +0900

    環境依存文字修正

--- src/Person.java
+++ src/Person.java
@@ -3,24 +3,24 @@ public class Person {
 	public String name = null;
 	public int age = 0;
 	
-	public Person() {}  //デフォルトコンストラクタ①。他の型のコンストラクタを宣言しない場合は不要
+	public Person() {}  //デフォルトコンストラクタ1。他の型のコンストラクタを宣言しない場合は不要
 	
-	public Person(String name, int age) {  //コンストラクタ②
+	public Person(String name, int age) {  //コンストラクタ2
 		this.name = name;
 		this.age = age;
 	}
 	
-	public Person(String name) {  //コンストラクタ③
+	public Person(String name) {  //コンストラクタ3
 		this.name = name;
 		this.age = 0;
 	}
 	
-	public Person(int age) {  //コンストラクタ④
+	public Person(int age) {  //コンストラクタ4
 		this.name = "名前なし";
 		this.age = age;
 	}
 	
-	public Person(int age, String name) {  //コンストラクタ⑤
+	public Person(int age, String name) {  //コンストラクタ5
 		this.name = name;
 		this.age = age;
 	}
--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 	public static void main(String[]args) {
 		
-		//コンストラクタ①↓
+		//コンストラクタ1↓
 		Person taro = new Person();
 		taro.name = "taro";
 		taro.age = 18;
@@ -10,25 +10,25 @@ public class Test {
 		System.out.println(taro.age);
 		System.out.println();
 		
-		//コンストラクタ②↓
+		//コンストラクタ2↓
 		Person jiro = new Person("jiro",20);
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
 		System.out.println();
 		
-		//コンストラクタ③↓
+		//コンストラクタ3↓
 		Person saburo = new Person("saburo");
 		System.out.println(saburo.name);
 		System.out.println(saburo.age);
 		System.out.println();
 		
-		//コンストラクタ④↓
+		//コンストラクタ4↓
 		Person nanashi = new Person(25);
 		System.out.println(nanashi.name);
 		System.out.println(nanashi.age);
 		System.out.println();
 		
-		//コンストラクタ⑤
+		//コンストラクタ5
 		Person hanako = new Person(17,"hanako");
 		System.out.println(hanako.name);
 		System.out.println(hanako.age);
